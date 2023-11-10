5170427e20b8ff4bb3f4a06762d3594c7d1b2639
9wynneth <gisviandhy@student.ciputra.ac.id>
2023-11-10 08:04:22 +0700

    Initial Commit

---
+++ Scryfall app W09/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scryfall app W09
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scryfall app W09/Scryfall_app_W09App.swift
@@ -0,0 +1,17 @@
+//
+//  Scryfall_app_W09App.swift
+//  Scryfall app W09
+//
+//  Created by MacBook Pro on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Scryfall_app_W09App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
