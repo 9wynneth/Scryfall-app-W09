c451366eef63e06cdf6962517ea1e83ecf812084
9wynneth <gisviandhy@student.ciputra.ac.id>
2023-11-10 10:05:18 +0700

    card detail still wrong, blm ada sort, ui blm same

--- Scryfall app W09/ContentView.swift
+++ Scryfall app W09/ContentView.swift
@@ -1,26 +1,138 @@
-//
-//  ContentView.swift
-//  Scryfall app W09
-//
-//  Created by MacBook Pro on 10/11/23.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct Card: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let image_uris: CardImageUris
+    let legalities: Legalities
+    
+}
+
+struct Legalities: Codable {
+    let standard: String
+    let future: String
+    let historic: String
+}
+
+
+struct CardImageUris: Codable {
+    let small: String
+}
+
+struct CardList: Codable {
+    let data: [Card]
+}
+
+
+
+struct CardDetail: View {
+    let card: Card
+
+    var legalitiesList: [String] {
+        let mirror = Mirror(reflecting: card.legalities)
+        return mirror.children.map { label, value in
+            "\(label): \(value)"
+        }
+    }
+
+    var legalityBackgroundColor: Color {
+        return cardLegalities.standard == "legal" ? Color.green : (cardLegalities.standard == "banned" ? Color.red : Color.gray)
+    }
+
+    var legalityTextColor: Color {
+        return cardLegalities.standard == "legal" ? Color.white : Color.black
+    }
+
+    var cardLegalities: Legalities {
+        return card.legalities
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            RemoteImage(url: card.image_uris.small)
+                .aspectRatio(contentMode: .fit)
+
+            Text(card.name)
+                .font(.title)
+                .padding()
+                .background(legalityBackgroundColor)
+                .foregroundColor(legalityTextColor)
+                .cornerRadius(10)
+                .padding(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(legalityTextColor, lineWidth: 1)
+                )
+
+            VStack(alignment: .leading, spacing: 5) {
+                ForEach(legalitiesList, id: \.self) { legality in
+                    Text(legality)
+                        .font(.title)
+                        .background(legalityBackgroundColor)
+                        .foregroundColor(legalityTextColor)
+                }
+            }
         }
-        .padding()
+        .navigationTitle(card.name)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+
+
+
+struct ContentView: View {
+    @State private var cards: [Card] = []
+    @State private var searchText = ""
+
+    var filteredCards: [Card] {
+        if searchText.isEmpty {
+            return cards
+        } else {
+            return cards.filter { $0.name.localizedCaseInsensitiveContains(searchText) }
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            ScrollView {
+
+                TextField("Search Cards", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())], spacing: 10) {
+                    ForEach(filteredCards) { card in
+                        NavigationLink(destination: CardDetail(card: card)) {
+                            VStack {
+                                RemoteImage(url: card.image_uris.small)
+                                    .frame(width: 100, height: 150) // Adjust the size as needed
+                                Text(card.name)
+                                    .font(.caption) // You can customize the font and style
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Magic Cards")
+            .onAppear {
+                if let url = Bundle.main.url(forResource: "WOT-Scryfall", withExtension: "json") {
+                    do {
+                        let data = try Data(contentsOf: url)
+                        let decoder = JSONDecoder()
+                        let decodedData = try decoder.decode(CardList.self, from: data)
+                        cards = decodedData.data
+                    } catch {
+                        print("Error loading JSON data: \(error)")
+                    }
+                } else {
+                    print("Failed to find JSON file")
+                }
+            }
+        }
     }
 }
+
+
+
